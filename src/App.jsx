128e611d20666a011457680bb1bbd44e0fc8dc07
raworiginal <randall.a.wolfe@gmail.com>
2025-08-08 10:26:06 -0700

    created Bookshelf component and import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Bookshelf from "./components/Bookshelf/Bookshelf.jsx";
 import "./App.css";
 
 const App = () => {
-	return <h1>Hello world!</h1>;
+	return (
+		<>
+			<h1>My Bookshelf</h1>
+			<Bookshelf />
+		</>
+	);
 };
 
 export default App;
