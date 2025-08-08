ad2e8b70f38edc3bfe187eb7c7fe51993bb01481
raworiginal <randall.a.wolfe@gmail.com>
2025-08-08 10:41:32 -0700

    defined intial state of books in bookshelf

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 const Bookshelf = () => {
+	const [books, setbooks] = useState([
+		{ title: "Dungeon Crawler Carl", author: "Matt Dinniman" },
+		{ title: "The Parable of The Sower", author: "Octavia Butler" },
+	]);
 	return (
 		<div className="bookshelfDiv">
 			<div className="formDiv">
