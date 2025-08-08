1475060abc60693e38063910d798a0f95ee96d8f
raworiginal <randall.a.wolfe@gmail.com>
2025-08-08 10:08:24 -0700

    fixed typo in setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+	return <h1>Hello world!</h1>;
 };
 
 export default App;
-
-
-export default App
