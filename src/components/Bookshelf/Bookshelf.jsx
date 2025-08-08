06c106ce2bd47dc2d86e8e2a34f9d5b49e4ce852
raworiginal <randall.a.wolfe@gmail.com>
2025-08-08 11:44:45 -0700

    added book card

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -32,30 +32,36 @@ const Bookshelf = () => {
 			<div className="formDiv">
 				<h3>Add a Book</h3>
 				<form onSubmit={handleSubmit}>
-					<div>
-						<label htmlFor="title">Title: </label>
-						<input
-							type="text"
-							name="title"
-							id="title"
-							value={newBook.title}
-							onChange={handleInputChange}
-						/>
-					</div>
-					<div>
-						<label htmlFor="author">Author:</label>
-						<input
-							type="text"
-							name="author"
-							id="author"
-							value={newBook.author}
-							onChange={handleInputChange}
-						/>
-					</div>
+					<label htmlFor="title">Title: </label>
+					<input
+						type="text"
+						name="title"
+						id="title"
+						value={newBook.title}
+						onChange={handleInputChange}
+					/>
+
+					<label htmlFor="author">Author:</label>
+					<input
+						type="text"
+						name="author"
+						id="author"
+						value={newBook.author}
+						onChange={handleInputChange}
+					/>
 					<button type="submit">Submit</button>
 				</form>
 			</div>
-			<div className="bookCardsDiv">{/* Book cards will display here */}</div>
+			<div className="bookCardsDiv">
+				{books.map((book, index) => {
+					return (
+						<div key={index} className="bookCard">
+							<h3>{book.title}</h3>
+							<p>{book.author}</p>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
